c81fa357bb171ac24dcabd444caaa52a142252a6
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-07-03 17:42:51 +0530

    test1

---
+++ test1.js
@@ -0,0 +1,9 @@
+const greet = function(greetings) {
+    return function(name) {
+        console.log(`${greetings} ${name}`);
+    }
+}
+
+const greeterhey = greet('hey');
+greeterhey('nick');
+greeterhey('jonas');
