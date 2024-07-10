a46e218bf89064835da8ba3dbdf6d4782b1bea92
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-07-10 20:17:26 +0530

    trim() method

---
+++ trimMethod.js
@@ -0,0 +1,10 @@
+// The trim() method in JavaScript is a built-in string method that removes whitespace characters from the beginning and end of a string. 
+// These whitespace characters include spaces, tabs, and newline characters12. To use it, simply call string.trim(). 
+// It won’t modify the original string but will return a new string with leading and trailing whitespaces removed. 
+// For example:
+
+let text = "   Hello, World!   ";
+console.log(text);
+
+let result = text.trim();
+console.log(result);
