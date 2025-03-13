77975529b834183447ba377cf92a1f2fcd5f7018
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2025-03-13 23:34:24 +0530

    used prettier

--- project/jscript.js
+++ project/jscript.js
@@ -2,20 +2,19 @@
 
 const randomColor = function () {
     const hex = "0123456789ABCDEF";
-    let color = '#';
+    let color = "#";
     for (let i = 0; i < 6; i++) {
         color += hex[Math.floor(Math.random() * 16)];
     }
     return color;
 };
-let intervalId
+let intervalId;
 const startChangingColor = function () {
-
     if (!intervalId) {
         intervalId = setInterval(changBgColor, 1000);
     }
     function changBgColor() {
-        document.body.style.backgroundColor = randomColor()
+        document.body.style.backgroundColor = randomColor();
     }
 };
 
@@ -24,12 +23,6 @@ const stopChangingColor = function () {
     intervalId = null;
 };
 
+document.querySelector("#start").addEventListener("click", startChangingColor);
 
-
-document.querySelector('#start').addEventListener
-    ('click', startChangingColor);
-
-document.querySelector('#stop').
-    addEventListener('click', stopChangingColor);
-
-    
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
