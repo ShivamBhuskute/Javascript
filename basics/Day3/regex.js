27847d6f3290b428d232650e90ce9261d17ac30c
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2025-03-17 23:14:41 +0530

    2nd exaple regex

--- basics/Day3/regex.js
+++ basics/Day3/regex.js
@@ -2,3 +2,9 @@ let text1 = "The quick brown fox jumps over the lazy dog.";
 let regex = /fox/
 
 console.log(regex.test(text1));
+console.log(text1.match(regex));
+
+let text2 = "abbcccdddd"
+let regex2 = /ab+c{3}d{4}/
+console.log(regex2.test(text2));
+
